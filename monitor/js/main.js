db09e94bf29e52863b7f44366d91f40089c00f49
Jose Alberto Sanchez Montiel <82729414+josesanchezmx@users.noreply.github.com>
2024-05-21 12:20:52 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -5,7 +5,7 @@ function callApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://18.219.22.224//lot-car-control/back-end/apis/getRegistro.php"
+      "https://3.134.104.32//lot-car-control/back-end/apis/getRegistro.php"
     )
     .then(function (response) {
       // manejar respuesta exitosa
